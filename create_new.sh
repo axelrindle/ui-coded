b6eba25592a39c0281df0de2ec57d98c7687c823
Axel Rindle <axel.rindle@gmx.de>
2018-05-20 18:32:48 +0200

    Add comments for clarity

--- create_new.sh
+++ create_new.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Die on error
 die () {
     echo >&2 "$@"
     exit 1
 }
 
+# Make sure one argument is provided
 [ "$#" -eq 1 ] || die "Provide a name for the new folder! ( ./create_new.sh MY_NEW_PROJECT )"
 
+# Copy the boilerplate to the new project folder
 rsync -av --progress project-boilerplate/ $1 --exclude node_modules
