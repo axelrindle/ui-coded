9438cf5161a0f1265b8999bda5568d44d8fcf149
Axel Rindle <axel.rindle@gmx.de>
2018-05-20 19:46:09 +0200

    Allow hyphens and underscores

--- create_new.sh
+++ create_new.sh
@@ -10,7 +10,7 @@ die () {
 [ "$#" -eq 1 ] || die "Provide a name for the new folder! ( ./create_new.sh MY_NEW_PROJECT )"
 
 # Validate argument to be an alphanumeric string
-! [[ "$1" =~ [^a-zA-Z0-9] ]] || die "'$1' is not alphanumeric!"
+! [[ "$1" =~ [^a-zA-Z0-9_-] ]] || die "'$1' must only contain letters, numbers, hyphens or underscores!"
 
 # Copy the boilerplate to the new project folder
 rsync -av --progress project-boilerplate/ $1 --exclude node_modules
