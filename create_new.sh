781a6271481dd237271bcd07c69a6345231fd265
Axel Rindle <axel.rindle@gmx.de>
2018-05-20 18:37:25 +0200

    Validate the argument to be alphanumeric

--- create_new.sh
+++ create_new.sh
@@ -9,5 +9,8 @@ die () {
 # Make sure one argument is provided
 [ "$#" -eq 1 ] || die "Provide a name for the new folder! ( ./create_new.sh MY_NEW_PROJECT )"
 
+# Validate argument to be an alphanumeric string
+! [[ "$1" =~ [^a-zA-Z0-9] ]] || die "'$1' is not alphanumeric!"
+
 # Copy the boilerplate to the new project folder
 rsync -av --progress project-boilerplate/ $1 --exclude node_modules
